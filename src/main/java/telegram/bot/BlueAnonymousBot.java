cb53609c72603184d86edc14a7c85eee7b1f6985
negar anabestani <negar.anabestani@gmail.com>
2021-12-07 23:19:34 +0330

    menu test buttons

--- src/main/java/telegram/bot/BlueAnonymousBot.java
+++ src/main/java/telegram/bot/BlueAnonymousBot.java
@@ -5,8 +5,13 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import telegram.UpdateHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlueAnonymousBot extends TelegramLongPollingBot {
     private final UpdateHandler updateHandler = new UpdateHandler();
@@ -22,8 +27,12 @@ public class BlueAnonymousBot extends TelegramLongPollingBot {
     }
 
     @Override
+
     public void onUpdateReceived(Update update) {
+        getMainMenuKeyboard();
         SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setReplyMarkup(getMainMenuKeyboard());
         sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
         String message;
         try {
@@ -37,6 +46,26 @@ public class BlueAnonymousBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+    private static ReplyKeyboardMarkup getMainMenuKeyboard() {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add("fuck it");
+        keyboardFirstRow.add("fuck life");
+//        KeyboardRow keyboardSecondRow = new KeyboardRow();
+//        keyboardSecondRow.add(getSettingsCommand(language));
+//        keyboardSecondRow.add(getRateCommand(language));
+        keyboard.add(keyboardFirstRow);
+//        keyboard.add(keyboardSecondRow);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+
+        return replyKeyboardMarkup;
     }
 
 
