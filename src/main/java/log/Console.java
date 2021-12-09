c3860dcc5eaf4fa4d0ebc0540d67139be36825de
unknown <farid_127@outlook.com>
2021-12-09 15:54:41 +0330

    add javaDoc to log.Console.java

--- src/main/java/log/Console.java
+++ src/main/java/log/Console.java
@@ -2,6 +2,10 @@ package log;
 
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+/**
+ * Console class
+ * this class used to print some information in console.
+ */
 public class Console {
     public static void printNewRequestInfo(Update update) {
         System.out.println("- new Request:" +
