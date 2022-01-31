1936babe032e5eadc483e44d787b2d174e120b21
negar anabestani <negar.anabestani@gmail.com>
2022-01-31 17:20:46 +0330

    started specific connection

--- src/main/java/telegram/command/SpecificConnectionCommand.java
+++ src/main/java/telegram/command/SpecificConnectionCommand.java
@@ -2,7 +2,12 @@ package telegram.command;
 
 public class SpecificConnectionCommand extends Command {
 
-
+    private static final String localMessage = "ربات \" ناشناس \" کاملا انحصاری فقط برای اعضای محترم کانال « رازکام » طراحی شده ، برای استفاده از ربات ، در کانال زیر عضو شوید \uD83D\uDC47\n" +
+            "\n" +
+            "\uD83D\uDD39 @Razcom   \uD83D\uDD39 @Razcom\n" +
+            "\uD83D\uDD39 @Razcom   \uD83D\uDD39 @Razcom\n" +
+            "\n" +
+            "بعد از عضویت در کانال روی « ✅ تایید عضویت » بزنید \uD83D\uDC47\uD83D\uDC47";
     public SpecificConnectionCommand(String chatId) {
         super(chatId);
     }
