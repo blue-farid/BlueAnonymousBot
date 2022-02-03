bbc9056d904ceab7c5c8cf88d62e415f00dd9462
blue_farid <farid_127@outlook.com>
2022-02-03 13:23:00 +0330

    impl TimeUtils

--- src/main/java/utils/TimeUtils.java
+++ src/main/java/utils/TimeUtils.java
@@ -1,4 +1,26 @@
 package utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TimeUtils {
+    private static TimeUtils instance;
+    private final SimpleDateFormat formatter;
+    private TimeUtils() {
+        formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    }
+
+    public static TimeUtils getInstance() {
+        if (instance == null)
+            instance = new TimeUtils();
+        return instance;
+    }
+
+    public Date getCurrentDate() {
+        return new Date();
+    }
+
+    public String getCurrentDateAndTimeString() {
+        return formatter.format(this.getCurrentDate());
+    }
 }
