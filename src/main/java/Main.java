52ab0088d8301a9d900a46df876302018e736ac5
blue_farid <farid_127@outlook.com>
2022-02-05 15:34:28 +0330

    version 1.4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,7 @@ import telegram.BlueAnonymousBot;
  * @author Farid Masjedi
  * @author Negar Anabestani
  * @author Alireza Jabbari
- * @version 1.1
+ * @version 1.4
  * <p>
  * Telegram_id: @blue_farid
  * github: https://github.com/blue-farid
