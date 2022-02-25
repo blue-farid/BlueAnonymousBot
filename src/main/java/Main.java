ab37d09f14a3ae3597657545bd812463df53fa04
Farid Masjedi <farid_127@outlook.com>
2022-02-25 13:38:55 +0330

    version 3.7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ import java.util.Properties;
  * @author Farid Masjedi
  * @author Negar Anabestani
  * @author Alireza Jabbari
- * @version 3.65
+ * @version 3.7
  * <p>
  * Telegram_id: @blue_farid
  * github: https://github.com/blue-farid
@@ -36,7 +36,7 @@ public class Main {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(BlueAnonymousBot.getInstance());
             new Thread(new ConsoleReader()).start();
-            log.Console.initMessage("3.65");
+            log.Console.initMessage("3.7");
         } catch (TelegramApiException e) {
             e.printStackTrace();
             System.exit(-1);
