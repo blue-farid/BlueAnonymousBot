154b35d7785872c8b04243f56dfc1267180b47fe
blue_farid <farid_127@outlook.com>
2022-03-23 16:47:22 +0430

    refactor: specific connection

--- src/main/java/telegram/command/SpecificConnectionCommand.java
+++ src/main/java/telegram/command/SpecificConnectionCommand.java
@@ -1,25 +1,22 @@
 package telegram.command;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
-import inlineMenu.InlineJoinChannelKeyBoard;
 import menu.CancelMenu;
 import model.Client;
 import model.ClientState;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import service.ClientService;
 import properties.Property;
+import service.ClientService;
 import telegram.BlueAnonymousBot;
 
 public class SpecificConnectionCommand extends Command {
 
     private final String localMessage;
 
-    private Client client;
+    private final Client client;
 
     public SpecificConnectionCommand(String chatId, Client client) {
         super(chatId);
         this.client = client;
-        localMessage= Property.MESSAGES_P.get("specific_connection");
+        localMessage = Property.MESSAGES_P.get("specific_connection");
     }
 
 
