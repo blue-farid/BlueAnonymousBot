615cb5489343e82e40b2e21b3ce7668d32032dd2
blue_farid <farid_127@outlook.com>
2022-03-23 16:57:05 +0430

    delete unused keybord

--- src/main/java/inlineMenu/InlineJoinChannelKeyBoard.java
+++
@@ -1,8 +0,0 @@
-package inlineMenu;
-
-public class InlineJoinChannelKeyBoard extends InlineBlueKeyBoard{
-    public InlineJoinChannelKeyBoard(){
-        addButtonToList(0,"✅ تایید عضویت"+"::"+"joinChannel");
-        setInlineBlueKeyBoard();
-    }
-}
