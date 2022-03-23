66c5ae6627520f9f8713f2caed9f8a5e9e0342da
blue_farid <farid_127@outlook.com>
2022-03-23 16:54:41 +0430

    ClientDao javadoc

--- src/main/java/dao/ClientDao.java
+++ src/main/java/dao/ClientDao.java
@@ -2,11 +2,9 @@ package dao;
 
 import model.Client;
 import model.ClientState;
-import utils.FileUtils;
 import utils.SQLiteUtils;
 
 import java.util.Collection;
-import java.util.HashMap;
 
 /**
  * the ClientDao class
@@ -20,10 +18,18 @@ public class ClientDao {
         return instance;
     }
 
+    /**
+     * @return Clients
+     */
     public Collection<Client> getClients() {
         return SQLiteUtils.getInstance().selectClients();
     }
 
+    /**
+     * add client
+     * @param client the client
+     * @return the result as int
+     */
     public int addClient(Client client) {
         if (!exist(client.getId())) {
             return SQLiteUtils.getInstance().insertClient(client);
@@ -32,44 +38,96 @@ public class ClientDao {
         }
     }
 
-    public boolean exist(long id){
+    /**
+     * check the existence of client with the specific id
+     * @param id the client id.
+     * @return existence boolean
+     */
+    public boolean exist(long id) {
         return SQLiteUtils.getInstance().selectClientChatId(id) != null;
     }
 
+    /**
+     * search for a client
+     * @param id the client id
+     * @return the client
+     */
     public Client searchById(long id) {
         return SQLiteUtils.getInstance().selectClient(id);
     }
 
+    /**
+     * search for a client
+     * @param username the client username
+     * @return the client
+     * @deprecated not efficient.
+     */
     @Deprecated
     public Client searchByUsername(String username) {
         Collection<Client> clientsCollection = SQLiteUtils.getInstance().selectClients();
-        for (Client client: clientsCollection) {
+        for (Client client : clientsCollection) {
             if (client.getTelegramUser().getUserName().equals(username))
                 return client;
         }
         return null;
     }
 
+    /**
+     * search for a client
+     * @param shortDeepLink the client's short deeplink
+     * @return the client.
+     */
     public Client searchByDeepLink(String shortDeepLink) {
         return SQLiteUtils.getInstance().selectClient(shortDeepLink);
     }
 
+    /**
+     * set new deeplink for the client.
+     * @param id the client's id.
+     * @param lDeepLink long deeplink
+     * @param shDeepLink short deeplink
+     * @return the result as int
+     */
     public int setDeepLink(long id, String lDeepLink, String shDeepLink) {
         return SQLiteUtils.getInstance().updateClientDeepLink(id, lDeepLink, shDeepLink);
     }
 
+    /**
+     * set admin ability boolean
+     * @param id the client's id
+     * @param admin the admin ability boolean
+     * @return the result as int
+     */
     public int setAdmin(long id, boolean admin) {
         return SQLiteUtils.getInstance().updateClientAdmin(id, admin);
     }
 
+    /**
+     * set contact for the client
+     * @param id the client's id
+     * @param contactId the contact id
+     * @return the result as int
+     */
     public int setContact(long id, long contactId) {
         return SQLiteUtils.getInstance().updateClientContact(id, contactId);
     }
 
+    /**
+     * set the client's state
+     * @param id the client's id
+     * @param clientState the client state
+     * @return the result as int
+     */
     public int setClientState(long id, ClientState clientState) {
         return SQLiteUtils.getInstance().updateClientState(id, clientState);
     }
 
+    /**
+     * set the client's contact message id
+     * @param id the client's id
+     * @param messageId the messgae id
+     * @return the result as int
+     */
     public int setContactMessageId(long id, Integer messageId) {
         return SQLiteUtils.getInstance().updateClientContactMessageId(id, messageId);
     }
