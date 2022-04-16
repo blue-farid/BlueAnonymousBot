c3a4d8b73c9ea0b9b444229f8b94fdfe587545c7
blue_farid <farid_127@outlook.com>
2022-04-16 10:49:12 +0430

    fix: block command

--- src/main/java/inlineMenu/InlineAMB.java
+++ src/main/java/inlineMenu/InlineAMB.java
@@ -8,7 +8,7 @@ public class InlineAMB extends InlineBlueKeyBoard {
 
     public InlineAMB(long senderId, int messageId) {
 
-        addButtonToList(0, "⛔️بلاک" + "::" + "block");
+        addButtonToList(0, "⛔️بلاک" + "::" + "block " + senderId);
         addButtonToList(0, "✍\uD83C\uDFFB پاسخ" + "::" + "answer " + senderId + " " + messageId);
         setInlineBlueKeyBoard();
     }
--- src/main/java/telegram/command/BlockCommand.java
+++ src/main/java/telegram/command/BlockCommand.java
@@ -6,18 +6,21 @@ import telegram.BlueAnonymousBot;
 
 public class BlockCommand extends Command {
     private final long contactId;
+    private final Client client;
 
-    public BlockCommand(String chatId, long contactId) {
+    public BlockCommand(String chatId, long contactId, Client client) {
         super(chatId);
         this.contactId = contactId;
+        this.client = client;
     }
 
     @Override
     public void execute() {
         Client contact = ClientDao.getInstance().searchById(this.contactId);
         if (contact.isAdmin()) {
-            this.sendMessage.setText("" + contact.getTelegramUser().getFirstName()
+            this.sendMessage.setText("" + client.getTelegramUser().getFirstName()
             + " tries to block you!");
+            this.sendMessage.setChatId(String.valueOf(contactId));
             BlueAnonymousBot.getInstance().executeSendMessage(this.sendMessage);
         } else {
             // who cares?
