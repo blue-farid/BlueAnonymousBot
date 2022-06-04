ec987f952c67fc99dc0a9ad508343640098dc3c5
blue_farid <farid_127@outlook.com>
2022-06-04 16:05:12 +0430

    version 6.0.3 - edit javadoc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ import java.util.Properties;
  * @author Farid Masjedi
  * @author Negar Anabestani
  * @author Alireza Jabbari
- * @version 6.0.2
+ * @version 6.0.3
  * <p>
  * Telegram_id: @blue_farid
  * github: https://github.com/blue-farid
@@ -47,7 +47,7 @@ public class Main {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(BlueAnonymousBot.getInstance());
             new Thread(new ConsoleReader()).start();
-            ConsoleWriter.initMessage("6.0.2");
+            ConsoleWriter.initMessage("6.0.3");
         } catch (TelegramApiException e) {
             e.printStackTrace();
             System.exit(-1);
