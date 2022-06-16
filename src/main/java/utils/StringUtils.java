23740dce5c43c0094790f55f09f5154f27853dbd
blue_farid <farid_127@outlook.com>
2022-06-16 12:17:21 +0430

    fix: printAllUser failed

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -1,7 +1,5 @@
 package utils;
 
-import com.google.common.base.Strings;
-
 import javax.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +7,9 @@ import java.util.List;
 @Singleton
 public class StringUtils {
     private static StringUtils instance;
-    private StringUtils() {}
+
+    private StringUtils() {
+    }
 
     public static StringUtils getInstance() {
         if (instance == null) {
@@ -22,9 +22,11 @@ public class StringUtils {
         List<String> strings = new ArrayList<>();
         int p = 0;
         while (p < str.length()) {
-            String toAdd = str.substring(p, len);
-            if (Strings.isNullOrEmpty(toAdd)) {
+            String toAdd;
+            if (len + p >= str.length()) {
                 toAdd = str.substring(p);
+            } else {
+                toAdd = str.substring(p, len + p);
             }
             strings.add(toAdd);
             p += len;
