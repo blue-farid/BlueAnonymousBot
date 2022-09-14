879347843e56c3cec3a0b114bb0cb672fe75adfd
blue_farid <farid_127@outlook.com>
2022-09-14 16:33:29 +0430

    initial remaster commit

---
+++ src/main/java/com/blue_farid/blue_anonymous_bot/BlueAnonymousBotApplication.java
@@ -0,0 +1,13 @@
+package com.blue_farid.blue_anonymous_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueAnonymousBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueAnonymousBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blue_farid/blue_anonymous_bot/BlueAnonymousBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blue_farid.blue_anonymous_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueAnonymousBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
