2fa5fa587d86cc19b660bfe249872b03e2afb3cc
blue_farid <farid_127@outlook.com>
2022-09-15 18:52:35 +0430

    add sqlite #55

---
+++ src/main/java/com/blue_farid/blue_anonymous_bot/model/ClientState.java
@@ -0,0 +1,8 @@
+package com.blue_farid.blue_anonymous_bot.model;
+
+/**
+ * the Client State class
+ */
+public enum ClientState {
+    NORMAL, SENDING_MESSAGE_TO_CONTACT, SENDING_CONTACT_INFO, CHOOSING_CONTACT_SEX, ADMIN_SENDING_CONTACT_ID
+}
