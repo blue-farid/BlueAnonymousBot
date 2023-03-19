b3a3493da77705b4aa346ac80474aab3b6142526
blue_farid <blue_farid@hotmail.com>
2023-03-20 02:40:06 +0330

    fix: security problem

--- src/main/java/com/blue_farid/blue_anonymous_bot/config/SecurityConfig.java
+++ src/main/java/com/blue_farid/blue_anonymous_bot/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.blue_farid.blue_anonymous_bot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -16,9 +17,18 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${admin.username}")
+    private String username;
+
+    @Value("${admin.password}")
+    private String password;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable().csrf().disable().authorizeRequests()
+        http.cors().disable().csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
                 .antMatchers("/api/blue-anonymous-bot/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and().httpBasic();
@@ -28,8 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("admin")
+                .username(username)
+                .password(password)
                 .roles("ADMIN")
                 .build();
 
