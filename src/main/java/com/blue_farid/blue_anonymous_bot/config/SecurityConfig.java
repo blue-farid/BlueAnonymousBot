3ec8bce26efb790519d90a7e0a045d3913a8cf1d
blue_farid <blue_farid@hotmail.com>
2023-03-19 23:21:31 +0330

    fix: security problem

--- src/main/java/com/blue_farid/blue_anonymous_bot/config/SecurityConfig.java
+++ src/main/java/com/blue_farid/blue_anonymous_bot/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.blue_farid.blue_anonymous_bot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,15 +18,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll();
+        http.cors().disable().csrf().disable().authorizeRequests()
+                .antMatchers("/api/blue-anonymous-bot/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and().httpBasic();
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser("admin").password("admin").roles("ADMIN");
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
