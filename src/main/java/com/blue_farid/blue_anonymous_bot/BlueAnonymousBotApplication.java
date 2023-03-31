3e17fdba8486dcca925df45fc1ac9ddd017551a9
blue_farid <blue_farid@hotmail.com>
2023-03-31 19:50:43 +0330

    refactor: remove unnecessary imports

--- src/main/java/com/blue_farid/blue_anonymous_bot/BlueAnonymousBotApplication.java
+++ src/main/java/com/blue_farid/blue_anonymous_bot/BlueAnonymousBotApplication.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 /**
  * ! The BlueAnonymousBot Project !
