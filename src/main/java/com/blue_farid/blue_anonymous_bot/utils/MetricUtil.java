c043bfa076333bd2c4c447e986cfcfdb900bdadd
blue_farid <blue_farid@hotmail.com>
2023-05-13 22:03:04 +0330

    feat: expose total_sendMessages

--- src/main/java/com/blue_farid/blue_anonymous_bot/utils/MetricUtil.java
+++ src/main/java/com/blue_farid/blue_anonymous_bot/utils/MetricUtil.java
@@ -7,12 +7,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class MetricUtil {
     private final Counter totalRequests;
+    private final Counter totalSendMessages;
 
     public MetricUtil(MeterRegistry registry) {
-        totalRequests = registry.counter("total_requests");
+        totalRequests = registry.counter("requests");
+        totalSendMessages = registry.counter("sendMessages");
     }
 
     public void incrementTotalRequests() {
         totalRequests.increment();
+    }
+
+    public void incrementTotalSendMessages() {
+        totalSendMessages.increment();
     }
 }
