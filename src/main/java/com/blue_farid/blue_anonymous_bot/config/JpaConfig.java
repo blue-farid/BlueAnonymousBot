38a913f8c62945111000f542b1c78e3e79b5aa14
blue_farid <blue_farid@hotmail.com>
2023-08-02 19:27:02 +0330

    fix: jpa config

--- src/main/java/com/blue_farid/blue_anonymous_bot/config/JpaConfig.java
+++ src/main/java/com/blue_farid/blue_anonymous_bot/config/JpaConfig.java
@@ -1,27 +1,10 @@
 package com.blue_farid.blue_anonymous_bot.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.blue_farid.blue_anonymous_bot.repository")
-@EnableTransactionManagement
 public class JpaConfig {
-    @Bean
-    public LocalEntityManagerFactoryBean entityManagerFactory() {
-        return new LocalEntityManagerFactoryBean();
-    }
 
-    @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        return transactionManager;
-    }
 }
