888a3752621ca9411966128620c7181b737f5b83
blue_farid <blue_farid@hotmail.com>
2023-08-25 19:31:21 +0330

    fix: metric error

--- src/main/java/com/blue_farid/blue_anonymous_bot/utils/metric/MetricUtil.java
+++ src/main/java/com/blue_farid/blue_anonymous_bot/utils/metric/MetricUtil.java
@@ -1,13 +1,7 @@
 package com.blue_farid.blue_anonymous_bot.utils.metric;
 
-import com.blue_farid.blue_anonymous_bot.annotation.Response;
-import com.blue_farid.blue_anonymous_bot.telegram.command.CommandService;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Component
 public class MetricUtil {
@@ -15,17 +9,7 @@ public class MetricUtil {
 
 
     public MetricUtil(MeterRegistry registry) {
-        List<String> tags = new ArrayList<>();
-        Arrays.stream(CommandService.class.getDeclaredMethods())
-                .filter(m -> m.isAnnotationPresent(Response.class))
-                .forEach(m -> tags.add(m.getName()));
-
-        String[] tagsArray = new String[tags.size()];
-
-        for (int i = 0; i < tags.size(); i++)
-            tagsArray[i] = tags.get(i);
-
-        this.request = new TagCounter("request", registry, tagsArray);
+        this.request = new TagCounter("request", registry, "type");
     }
 
     public void increment(String tag) {
